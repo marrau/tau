78f606faf884c4eddbd08ca03782ee8d382bda3d
Martin Rau <martin.rau@braintags.de>
2020-10-08 08:16:39 +0200

    replace go-cmd with the native os/exec

--- pkg/shell/execute.go
+++ pkg/shell/execute.go
@@ -3,10 +3,9 @@ package shell
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
-	"time"
 
-	"github.com/go-cmd/cmd"
 	"github.com/go-errors/errors"
 
 	"github.com/avinor/tau/pkg/helpers/ui"
@@ -18,62 +17,53 @@ func Execute(options *Options, command string, args ...string) error {
 		options = &Options{}
 	}
 
-	// Disable output buffering, enable streaming
-	cmdOptions := cmd.Options{
-		Buffered:  false,
-		Streaming: true,
-	}
+	comma := exec.Command(command, args...)
+	comma.Env = os.Environ()
 
-	execCmd := cmd.NewCmdOptions(cmdOptions, command, args...)
 	if options.WorkingDirectory != "" {
-		execCmd.Dir = options.WorkingDirectory
+		comma.Dir = options.WorkingDirectory
 	}
-
-	execCmd.Env = os.Environ()
 
 	if len(options.Env) > 0 {
 		for k, v := range options.Env {
 			variable := fmt.Sprintf("%s=%s", k, v)
-			execCmd.Env = append(execCmd.Env, variable)
+			comma.Env = append(comma.Env, variable)
 		}
 	}
 
-	ui.Debug("environment variables: %#v", execCmd.Env)
-	ui.Debug("command: %s %s", execCmd.Name, strings.Join(execCmd.Args, " "))
+	ui.Debug("environment variables: %#v", comma.Env)
+	ui.Debug("command: %s %s", comma.Path, strings.Join(comma.Args, " "))
 
-	// Print STDOUT and STDERR lines streaming from Cmd
-	go func() {
-		for {
-			select {
-			case line := <-execCmd.Stdout:
-				processLine(options.Stdout, line)
-			case line := <-execCmd.Stderr:
-				processLine(options.Stderr, line)
+	comma.Stdout = &executeOutputProcess{
+		processors: options.Stdout,
+	}
+	comma.Stderr = &executeOutputProcess{
+		processors: options.Stderr,
+	}
+
+	err := comma.Run()
+	if err != nil {
+		if errorState, ok := err.(*exec.ExitError); ok {
+			if errorState.ExitCode() != 0 {
+				return errors.Errorf("%s command exited with exit code %v", command, errorState.ExitCode())
 			}
+		} else {
+			return err
 		}
-	}()
-
-	status := <-execCmd.Start()
-
-	for len(execCmd.Stdout) > 0 || len(execCmd.Stderr) > 0 {
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	if status.Error != nil {
-		return status.Error
-	}
-
-	if status.Exit != 0 {
-		return errors.Errorf("%s command exited with exit code %v", command, status.Exit)
 	}
 
 	return nil
 }
 
-func processLine(processors []OutputProcessor, line string) {
-	for _, out := range processors {
-		if !out.Write(line) {
+type executeOutputProcess struct {
+	processors []OutputProcessor
+}
+
+func (eop *executeOutputProcess) Write(p []byte) (n int, err error) {
+	for _, out := range eop.processors {
+		if !out.Write(string(p)) {
 			return
 		}
 	}
+	return len(p), nil
 }
